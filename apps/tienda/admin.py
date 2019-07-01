52854b9432b2906b4553a7003de5c8537bdbfce9
José Reinoza <josereid18@gmail.com>
2019-07-01 14:41:22 -0430

    Retocando modelos Producto, Modelo y Carrito
    Se modificaron los modelos "Producto" y "Modelo" cambiando relaciones.
    
    También se creó la clase "Carrito" para el carrito de compras.

--- apps/tienda/admin.py
+++ apps/tienda/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Producto
 from .models import Modelo
+from .models import Carrito
 
 admin.site.register(Producto)
 admin.site.register(Modelo)
+admin.site.register(Carrito)
--- apps/tienda/models.py
+++ apps/tienda/models.py
@@ -9,12 +9,23 @@ class Producto(models.Model):
         ('MO', 'Morral'),
     )
 
-    tipo_producto = models.CharField(max_length = 2, choices = TIPO_PRODUCTO)
-    nombre_producto = models.CharField(max_length = 100, blank = False, null = False)
-    descripcion = models.TextField(blank = False, null = False)
+    tipo_producto = models.CharField('Tipo de producto', max_length = 2, choices = TIPO_PRODUCTO)
+    nombre_producto = models.CharField('Nombre del producto', max_length = 100, blank = False, null = False)
+    descripcion = models.TextField('Descripción', blank = False, null = False)
+
+    def __str__(self):
+        return self.nombre_producto
 
 class Modelo(models.Model):
 
     producto = models.ForeignKey(Producto, on_delete = models.CASCADE)
     modelo = models.CharField(max_length = 50, blank = False, null = False)
     cantidad = models.IntegerField(blank = False, null = True)    
+
+    def __str__(self):
+        return self.modelo        
+
+class Carrito(models.Model):
+
+    cant_productos = models.IntegerField('Productos en el carrito', blank = True, null = True)
+    producto_en_carrito = models.ManyToManyField(Producto, blank = True)
