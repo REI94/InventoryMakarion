51c6db78f5c24c62165ab95d731ad41335a7227d
José Reinoza <josemrd1994@gmail.com>
2019-07-03 22:43:35 -0430

    Limpiando master para subir MakarionWeb-Django

--- apps/tienda/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Cliente
-
-class ClienteForm(forms.ModelForm):
-    class Meta:
-        model = Cliente
-        fields = ['nombre','apellido','tipo_documento_id','num_documento_id','edad','telefono','direccion','email','contrasenia']
--- apps/tienda/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import registrarCliente
-#from .views import home
-
-urlpatterns = [
-    #path('', home, name = 'index'),
-    path('registro/', registrarCliente, name = 'registro')
-]
